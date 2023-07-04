1ae3d62f3aa04131ffeb90dba465391e88a3912d
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2023-07-04 11:54:06 +0000

    changes about func with two args

--- src/Functions/array/groupSortedArray.cpp
+++ src/Functions/array/groupSortedArray.cpp
@@ -1,3 +1,4 @@
+#include <utility>
 #include <Functions/IFunction.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/FunctionHelpers.h>
@@ -18,6 +19,7 @@
 #include <Columns/ColumnDecimal.h>
 #include <Columns/ColumnNullable.h>
 #include <Columns/ColumnTuple.h>
+#include "Common/Exception.h"
 #include <Common/HashTable/ClearableHashMap.h>
 #include <Common/assert_cast.h>
 #include "Columns/IColumn.h"
@@ -96,7 +98,7 @@ private:
     UnpackedArrays prepareArrays(const ColumnsWithTypeAndName & columns, ColumnsWithTypeAndName & initial_columns) const;
 
     template <typename Map, typename ColumnType, bool is_numeric_column>
-    ColumnPtr executeOneParam(int length, const UnpackedArrays & array, MutableColumnPtr result_data_ptr)
+    ColumnPtr executeOneParam(size_t length, const UnpackedArrays & array, MutableColumnPtr result_data_ptr)
     {
         auto args = array.args.size();
         auto rows = array.base_rows;
@@ -124,9 +126,8 @@ private:
         auto null_map_column = ColumnUInt8::create();
         NullMap & null_map = assert_cast<ColumnUInt8 &>(*null_map_column).getData();
 
-        Arena arena;
-
         Map map;
+        Array res;
         std::vector<size_t> prev_off(args, 0);
         size_t result_offset = 0;
 
@@ -153,12 +154,166 @@ private:
                     {
                         value = &map[columns[arg_num]->getElement(i)];
                     }
-                    Array res;
                     res[i] = map[columns[arg_num]->getElement(i)];
                 }
-            }                
+            }
+            prev_off[arg_num] = off;
+            if (arg.is_const)
+                prev_off[arg_num] = 0;
+
+            if (!current_has_nullable)
+                all_has_nullable = false;
         }
+        if (all_has_nullable)
+        {
+            ++result_offset;
+            result_data.insertDefault();
+            null_map.push_back(1);
+        }
+            
+        for (size_t i = 0; i < res.size(); ++i)
+        {
+            for (size_t j = 0; j < res.size(); ++j)
+            {
+                std::swap(res[j], res[j+1]);
+            }
+        }
+        for (size_t i = 0; i < res.size(); i++)
+        {
+            if (i == length)
+                break;
+            ++result_offset;
+            if constexpr (is_numeric_column)
+                result_data.insertValue(res[i]);
+            
+            if (all_nullable)
+                null_map.push_back(0);
+        }
+        result_offsets.getElement(0) = result_offset;
         
+        ColumnPtr result_column = std::move(result_data_ptr);
+        if (all_nullable)
+            result_column = ColumnNullable::create(result_column, std::move(null_map_column));
+        return ColumnArray::create(result_column, std::move(result_offsets_ptr));
+    }
+
+    template <typename Map, typename ColumnType, bool is_numeric_column>
+    ColumnPtr executeTwoParams(size_t length, const UnpackedArrays & arrays, MutableColumnPtr result_data_ptr)
+    {
+        auto args = arrays.args.size();
+        auto rows = arrays.base_rows;
+        if (rows != 2)
+            throw Exception(ErrorCodes::BAD_ARGUMENTS, "Incorrect number of arguments");
+
+        bool all_nullable = true;
+
+        std::vector<const ColumnType *> columns;
+        columns.reserve(args);
+        for (const auto & arg : arrays.args)
+        {
+            if constexpr (std::is_same_v<ColumnType, IColumn>)
+                columns.push_back(arg.nested_column);
+            else
+                columns.push_back(checkAndGetColumn<ColumnType>(arg.nested_column));
+
+            if (!columns.back())
+                throw Exception(ErrorCodes::LOGICAL_ERROR, "Unexpected array type for function arrayIntersect");
+
+            if (!arg.null_map)
+                all_nullable = false;
+        }
+
+        auto & result_data = static_cast<ColumnType &>(*result_data_ptr);
+        auto result_offsets_ptr = ColumnArray::ColumnOffsets::create(rows);
+        auto & result_offsets = assert_cast<ColumnArray::ColumnOffsets &>(*result_offsets_ptr);
+        auto null_map_column = ColumnUInt8::create();
+        NullMap & null_map = assert_cast<ColumnUInt8 &>(*null_map_column).getData();
+
+        Map map;
+        std::vector<size_t> prev_off(args, 0);
+        Array res, arr1, arr2;        
+        size_t result_offset = 0;
+        for (size_t row = 0; row < rows; ++row)
+        {
+            map.clear();
+
+            bool all_has_nullable = all_nullable;
+
+            for (size_t arg_num = 0; arg_num < args; ++arg_num)
+            {
+                const auto & arg = arrays.args[arg_num];
+                bool current_has_nullable = false;
+
+                size_t off;
+                // const array has only one row
+                if (arg.is_const)
+                    off = (*arg.offsets)[0];
+                else
+                    off = (*arg.offsets)[row];
+
+                for (auto i : collections::range(prev_off[arg_num], off))
+                {
+                    if (arg.null_map && (*arg.null_map)[i])
+                        current_has_nullable = true;
+                    else if (!arg.overflow_mask || (*arg.overflow_mask)[i] == 0)
+                    {
+                        typename Map::mapped_type * value = nullptr;
+
+                        if constexpr (is_numeric_column)
+                        {
+                            res[i] = &map[columns[arg_num]->getElement(i)];
+                        }
+                    }
+                }
+
+                prev_off[arg_num] = off;
+                if (arg.is_const)
+                    prev_off[arg_num] = 0;
+
+                if (!current_has_nullable)
+                    all_has_nullable = false;
+            }
+            for (auto i = 0; i < int(res.size()/2); ++i)
+            {
+                arr1[i] = res[i];
+                arr2[i] = res[i + int(res.size() / 2)];
+            }
+            if (all_has_nullable)
+            {
+                ++result_offset;
+                result_data.insertDefault();
+                null_map.push_back(1);
+            }
+
+            for (const auto & pair : map)
+            {
+                if (pair.getMapped() == args)
+                {
+                    ++result_offset;
+                    if constexpr (is_numeric_column)
+                        result_data.insertValue(pair.getKey());
+                    else if constexpr (std::is_same_v<ColumnType, ColumnString> || std::is_same_v<ColumnType, ColumnFixedString>)
+                        result_data.insertData(pair.getKey().data, pair.getKey().size);
+                    else
+                        result_data.deserializeAndInsertFromArena(pair.getKey().data);
+
+                    if (all_nullable)
+                        null_map.push_back(0);
+                }
+            }
+            result_offsets.getElement(row) = result_offset;
+        }
+
+        ColumnPtr result_column = std::move(result_data_ptr);
+        if (all_nullable)
+            result_column = ColumnNullable::create(result_column, std::move(null_map_column));
+        return ColumnArray::create(result_column, std::move(result_offsets_ptr));
     }
 };
+
+REGISTER_FUNCTION(ArrayIntersect)
+{
+    factory.registerFunction<FunctionGroupSortedArray>();
+}
+
 }
